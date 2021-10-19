36865e85018cfc7b4190c2c9125ef93576df4f21
Annalisa <annalisa.bauer1@gmail.com>
2021-10-19 15:49:46 -0700

    backend routes

--- models/workout.js
+++ models/workout.js
@@ -2,18 +2,41 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
-name: {
-   type: String
-},
-exercise: [
-   {
-      type: Schema.Types.ObjectId,
-      ref: "Workout"
-   }
-]
+const workoutSchema = new Schema({
+   day:{
+      type: Date,
+      default: () => new Date()
+   },
+   exercises:[
+      {
+         type:{
+            type:String,
+            required: " Enter a Workout Type!"
+         },
+         name:{
+            type: String,
+            required: "Enter a Exercise Name!"
+         },
+         duration:{
+            type:Number,
+            required: "Enter an exercise duration time in minutes"
+         },
+         weight:{
+            type: Number
+         },
+         reps:{
+            type: Number
+         },
+         sets: {
+            type: Number
+         }, 
+         distance:{
+            type: Number
+         }
+      }
+   ]
 });
 
-const Workout = mongoose.model("Workout", WorkoutSchema);
+const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const db = require("./models");
-
 const app = express();
 
 app.use(logger("dev"));
@@ -15,17 +13,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
-
-app.get("/workouts", (req, res) => {
-     db.Workout.find({})
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.json(err);
-        });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify:false,
+  useUnifiedTopology: true
 });
+
+//routes
+app.use(require("./route/html"));
+app.use(require("./route/api"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
